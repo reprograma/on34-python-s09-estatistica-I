f8ff23bfe7769be68e13b88a6d96264ba00f960e
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2024-07-30 22:53:32 -0300

    Exercício 3 média aritmética

---
+++ exercicios/para-casa/Lethicia-Asevedo/exercicio3.py
@@ -0,0 +1,9 @@
+#Exercício 3: Pratique o cálculo de média, mediana e moda usando Python
+#Dado um conjunto de números, calcule a média aritmética desses números.
+#Conjunto: `[10, 20, 30, 40, 50]`
+
+media = (10+20+30+40+50) / 5
+
+print(f"A média aritmética do conjunto de números é: {media}")
+
+
