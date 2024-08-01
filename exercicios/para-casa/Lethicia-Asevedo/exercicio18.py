2f0f4c9c7f3cbe3997a37f4d1c043faeed31dd90
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2024-08-01 00:49:56 -0300

    Probabilidade indepentende do exercício 18

---
+++ exercicios/para-casa/Lethicia-Asevedo/exercicio18.py
@@ -0,0 +1,15 @@
+#Exercício 18: Você escolhe uma peça de roupa aleatoriamente de um armário
+#e, em seguida, escolhe uma peça de calçado aleatoriamente de um sapateiro.
+#Se o armário tem 10 camisetas e o sapateiro tem 8 pares de sapato, qual é a
+#probabilidade de escolher uma camiseta de cor azul e um par de sapatos
+#pretos?
+
+prob_camiseta = 1/10
+prob_sapato = 1/8
+
+prob_total = (prob_camiseta * prob_sapato) * 100
+
+print(f"A probabilidade de escolher uma camiseta azul e um par de sapatos pretos é de: {prob_total:.2f}")
+
+
+
